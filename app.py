3050ba05f86d482282a676465da18d93ba7207fd
Shinyehh <shinypikachu1234@gmail.com>
2024-11-29 19:24:18 -0800

    Test

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def login_required(f):
     return decorated_function
 
 # Configure application
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
